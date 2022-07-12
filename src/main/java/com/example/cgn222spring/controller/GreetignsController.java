77ebb86258c8fb2cf429e6ed935f58b0b3e0a123
Dominic <andro5@web.de>
2022-07-12 10:31:24 +0200

    pathVariable + RequestParam + PostMapping added

--- src/main/java/com/example/cgn222spring/controller/GreetignsController.java
+++ src/main/java/com/example/cgn222spring/controller/GreetignsController.java
@@ -1,15 +1,39 @@
 package com.example.cgn222spring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetignsController {
 
-    @GetMapping
+    String name = "";
+
+    @GetMapping()
     public String sayHello(){
-        return "Grüß Gott!";
+        return "Grüß Gott! " +name;
     }
+
+    @GetMapping("/en")
+    public String sayHello2(){
+        return "Hello!";
+    }
+
+    @GetMapping(path = "{name}")
+    public String sayHello3(@PathVariable String name, @RequestParam String q){
+        return q + " " + name;
+    }
+
+    @PostMapping
+    public String postName(@RequestBody String newName){
+        name = newName;
+        return "New name equals = " +name;
+    }
+
+
+
+    @GetMapping("/Daniel")
+    public String sayDaniel(){
+        return "Servus Daniel";
+    }
+
 }
